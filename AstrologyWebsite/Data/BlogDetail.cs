169055ad0c76307a1eac4f095b23deaa53190a2d
huongpham5515 <phamthuhuong892003@gmail.com>
2025-04-25 13:01:59 +0700

    Database Connection

---
+++ AstrologyWebsite/Data/BlogDetail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AstrologyWebsite.Data;
+
+public partial class BlogDetail
+{
+    public int Id { get; set; }
+
+    public int? BlogId { get; set; }
+
+    public virtual Blog? Blog { get; set; }
+}
