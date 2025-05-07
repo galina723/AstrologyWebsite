fceab6c2e34e84fb4e76a9046e9a40c35e44f661
huongpham5515 <phamthuhuong892003@gmail.com>
2025-05-07 17:44:05 +0700

    Clean up tracked files now ignored by .gitignore

---
+++ AstrologyWebsite/Models/TarotReader.cs
@@ -0,0 +1,11 @@
+﻿namespace AstrologyWebsite.Models
+{
+    public class TarotReader
+    {
+        public int Experience {  get; set; }
+        public string AstroUserId { get; set; }
+        public AstroUser Reader { get; set; }
+
+
+    }
+}
