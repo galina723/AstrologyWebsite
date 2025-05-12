e02cc0fe18f873936f64fc42b0dac4b4716e8dc6
hoanggyen <147621826+galina723@users.noreply.github.com>
2025-05-12 09:20:07 +0700

    feat: CRUD admin blog

--- AstrologyWebsite/Controllers/Admin/AdminController.cs
+++ AstrologyWebsite/Controllers/Admin/AdminController.cs
@@ -1,7 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AstrologyWebsite.Migrations;
+using System.Text.RegularExpressions;
+using AstrologyWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Humanizer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 public class AdminController : Controller
 {
+    SqlConnection con = new SqlConnection("Data Source=DESKTOP-C9H42P0;Initial Catalog=AstrologyDatabase;Integrated Security=True;Trust Server Certificate=True");
+
     public IActionResult Index()
     {
         return View();
@@ -30,8 +39,161 @@ public class AdminController : Controller
     {
         return View("~/Views/Admin/Blogs/CreateBlog.cshtml");
     }
-    public IActionResult EditBlog()
+    public IActionResult EditBlog(int? id)
     {
-        return View("~/Views/Admin/Blogs/EditBlog.cshtml");
+        if (id == null)
+        { 
+            return BadRequest(); 
+        }
+
+
+        List<Blog> blogs = new List<Blog>();
+
+        String sqlQuery = "SELECT * FROM Blogs";
+
+        con.Open();
+
+        SqlCommand sc = new SqlCommand(sqlQuery, con);
+
+        SqlDataReader rd = sc.ExecuteReader();
+
+        if (rd.Read())
+        {
+            while (rd.Read())
+            {
+                if(Convert.ToInt32(rd["Id"]) == id)
+                {
+                    DateTime date = Convert.ToDateTime(rd["CreatedDate"]);
+
+                    blogs.Add(new Blog
+                    {
+                        Id = Convert.ToInt32(rd["Id"]),
+                        Title = rd["Title"].ToString(),
+                        Content = rd["Content"].ToString(),
+                        CreatedDate = date,
+                        AuthorId = Convert.ToInt32(rd["AuthorId"]),
+                    });
+                }
+                
+            }
+        }
+
+        con.Close();
+
+        if (blogs.IsNullOrEmpty())
+        {
+            return NotFound();
+        }
+
+        return View("~/Views/Admin/Blogs/EditBlog.cshtml", blogs[0]);
+    }
+
+    [HttpPost]
+    public IActionResult EditBlog(Blog blog)
+    {
+        if (ModelState.IsValid)
+        { 
+
+            DateTime createTime = DateTime.Now;
+            String sqlQuery = "UPDATE Blogs SET title = '" + blog.Title + "', content = '" + blog.Content + "' WHERE Id = " + blog.Id;
+
+            con.Open();
+
+            SqlCommand sc = new SqlCommand(sqlQuery, con);
+
+            sc.ExecuteNonQuery();
+
+            con.Close();
+
+            return RedirectToAction("Blogs");
+        }
+
+        return View("~/Views/Admin/Blogs/Blogs.cshtml");
+    }
+
+    public IActionResult AddReader(string fullName, int gender, string phone, string email, DateTime dob)
+    {
+        // String SqlQuery = "INSERT INTO User"
+
+        return View("~/Views/Admin/Readers/Readers.cshtml");
+    }
+    public IActionResult AddBlog(String title, String content)
+    {
+        if (ModelState.IsValid)
+        {
+            DateTime createTime = DateTime.Now;
+            String sqlQuery = "INSERT INTO Blogs (title, content, createdDate, authorId) VALUES ('" + title + "', '" + content + "', '" + createTime + "', '" + 1 + "')";
+
+            con.Open();
+
+            SqlCommand sc = new SqlCommand(sqlQuery, con);
+
+            sc.ExecuteNonQuery();
+
+            con.Close();
+
+            return RedirectToAction("Blogs");
+        }
+
+        return View("~/Views/Admin/Blogs/Blogs.cshtml");
+    }
+
+    [Route("Admin/Blogs")]
+    public IActionResult GetBlog()
+    {
+        List<Blog> blogs = new List<Blog>();
+
+        String sqlQuery = "SELECT * FROM Blogs";
+
+        con.Open();
+
+        SqlCommand sc = new SqlCommand(sqlQuery, con);
+
+        SqlDataReader rd = sc.ExecuteReader();
+
+        if (rd.Read())
+        {
+            while (rd.Read())
+            {
+                DateTime date = Convert.ToDateTime(rd["CreatedDate"]);
+                String contentT = Regex.Replace(rd["Content"].ToString(), "<.*?>", String.Empty);
+                String content = contentT.Truncate(35);
+
+                blogs.Add(new Blog
+                {
+                    Id = Convert.ToInt32(rd["Id"]),
+                    Title = rd["Title"].ToString(),
+                    Content = content,
+                    CreatedDate = date,
+                    AuthorId = Convert.ToInt32(rd["AuthorId"]),
+                });
+            }
+        }
+
+        con.Close();
+
+        return View("~/Views/Admin/Blogs/Blogs.cshtml", blogs);
+    }
+
+    [HttpPost]
+    public IActionResult DeleteBlog(String id)
+    {
+
+        if (ModelState.IsValid)
+        {
+            String sqlQuery = "DELETE FROM Blogs WHERE ID = '" + id + "'";
+
+            con.Open();
+
+            SqlCommand sc = new SqlCommand(sqlQuery, con);
+
+            sc.ExecuteNonQuery();
+
+            con.Close();
+
+            return RedirectToAction("Blogs");
+        }
+
+        return View("~/Views/Admin/Blogs/Blogs.cshtml");
     }
 }
--- AstrologyWebsite/obj/Debug/net8.0/AstrologyWebsite.AssemblyInfo.cs
+++ AstrologyWebsite/obj/Debug/net8.0/AstrologyWebsite.AssemblyInfo.cs
@@ -10,10 +10,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("AstrologyWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0fb9882d066f8a4b29f1da45ee4bafd0a430957")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00c371ccd4040c8eedb299b4d77562013cbb6c8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AstrologyWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AstrologyWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
